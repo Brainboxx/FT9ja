09e3db5e63f10f82a1a86bfa3683d7c5b3118665
user <chineduudochuku66@gmail.com>
2023-06-21 20:15:18 +0100

    production ready

--- FT9ja/settings.py
+++ FT9ja/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+78)d&^2t3c+blrexkng_hu4y+n=vync@b)b=a)l=p-d0zsqra'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
